c2a2364faead091e618f626850a949a0b56793ab
Tomasz Kołakowski <01171631@pw.edu.pl>
2025-11-26 15:27:12 +0100

    Rozwiązanie

--- foo.hpp
+++ foo.hpp
@@ -8,8 +8,16 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
-
+    std::vector< char > retval;
+    auto f = [&](Human& h) {
+        h.birthday();
+        if (h.isMonster() == true) {
+            retval.push_back('n');
+        } else {
+            retval.push_back('y');
+        }
+    };
+    std::for_each(people.rbegin(), people.rend(), f );
     // Twoja implementacja tutaj
 
     return retval;
